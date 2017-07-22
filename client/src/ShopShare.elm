205c7446d733f212e48dee6ebf5f12c9b3f468fb
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2017-07-22 16:35:34 +1200

    add type aliases for ShoppingList and ListItem
    * ListItem is a text string and a boolean indicating completion
    * ShoppingList is just a list of ListItem
    
    * Added ViewShoppingList and ViewListItem as functions

--- client/src/ShopShare.elm
+++ client/src/ShopShare.elm
@@ -8,12 +8,22 @@ import Html.Attributes exposing (..)
 
 
 type alias Model =
-    {}
+    { shopping_lists : List ShoppingList }
+
+
+type alias ShoppingList =
+    { name : String, list_items : List ListItem }
+
+
+type alias ListItem =
+    { text : String
+    , completed : Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    {} ! []
+    { shopping_lists = [{name = "", list_items =[]}] } ! []
 
 
 
@@ -39,46 +49,25 @@ view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text "Hello shop share!" ]
-        , div [id "create-list-form"] [Html.form [class "form"] [input [placeholder "enter some text"] []]]
-        , ol [ id "lists" ]
-            [ li []
-                [ text "Bunnings"
-                , div [ id "Bunnings" ]
-                    [ text "Bunnings"
-                    , div [id "bunnings-add-item-form"] [Html.form [class "form"] [input [placeholder "enter some text"] []]]
-                    , ul [ class "list" ]
-                        [ li [] [ text "item1" ]
-                        , li [] [ text "item2" ]
-                        , li [] [ text "item3" ]
-                        ]
-                    ]
-                ]
-            , li []
-                [ text "Groceries"
-                , div [ id "Groceries" ]
-                    [ text "Groceries"
-                    , div [id "groceries-add-item-form"] [Html.form [class "form"] [input [placeholder "enter some text"] []]]
-                    , ul [ class "list" ]
-                        [ li [] [ text "item1" ]
-                        , li [] [ text "item2" ]
-                        , li [] [ text "item3" ]
-                        ]
-                    ]
-                ]
-            , li []
-                [ text "Home"
-                , div [ id "Home" ]
-                    [ text "Home"
-                    , div [id "home-add-item-form"] [Html.form [class "form"] [input [placeholder "enter some text"] []]]
-                    , ul [ class "list" ]
-                        [ li [] [ text "item1" ]
-                        , li [] [ text "item2" ]
-                        , li [] [ text "item3" ]
-                        ]
-                    ]
-                ]
+        , div [ id "create-list-form" ] [ Html.form [ class "form" ] [ input [ placeholder "enter some text" ] [] ] ]
+        , ol [ id "lists" ] (List.map viewShoppingList model.shopping_lists)
+        ]
+
+
+viewShoppingList : ShoppingList -> Html Msg
+viewShoppingList list =
+    li []
+        [ text list.name
+        , div []
+            [ input [ placeholder "enter some text" ] []
+            , ul [ class "list" ] (List.map viewListItem list.list_items)
             ]
         ]
+
+
+viewListItem : ListItem -> Html Msg
+viewListItem item =
+    li [] [ text item.text, input [ type_ "checkbox", name "list-item", value item.text ] [] ]
 
 
 subscriptions : Model -> Sub Msg
