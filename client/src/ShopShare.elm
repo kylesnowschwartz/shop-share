280ce4b59ec8cd91734edb9ea60401a4d0c77b1a
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2017-08-17 12:23:29 +1200

    Allow the editing of list items
    This commit fills in the ItemEdited message. Now list items while being edited will change their text.

--- client/src/ShopShare.elm
+++ client/src/ShopShare.elm
@@ -3,6 +3,7 @@ module ShopShare exposing (Model, Msg, init, subscriptions, update, view)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import List.Extra exposing (..)
 
 
 -- MODEL
@@ -58,39 +59,47 @@ init =
 type Msg
     = ShoppingListNameEdited Int String
     | ItemAdded Int String
-    | ItemTextEdited
+    | ItemEdited Int Int String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ShoppingListNameEdited updatedId newName ->
-            { model | shoppingLists = updateShoppingList model updatedId (updateName newName) } ! []
+        ShoppingListNameEdited updatedListId newName ->
+            { model | shoppingLists = updateShoppingList model updatedListId (updateName newName) } ! []
 
-        ItemAdded updatedId newItem->
-            { model | shoppingLists = updateShoppingList model updatedId (addItem newItem) } ! []
+        ItemAdded updatedListId newItem ->
+            { model | shoppingLists = updateShoppingList model updatedListId (addItem newItem) } ! []
 
-        ItemTextEdited ->
-            model ! []
+        ItemEdited updatedListId newItemId newItemText ->
+            { model | shoppingLists = updateShoppingList model updatedListId (editItem newItemText newItemId) } ! []
 
 
 updateName : String -> ShoppingList -> ShoppingList
 updateName newName list =
     { list | name = newName }
 
+
 addItem : String -> ShoppingList -> ShoppingList
 addItem newItem list =
-  { list | listItems = list.listItems ++ [{id = (incrementItemId list), text = newItem, completed = False}] }
-  -- TODO
-  -- this is currently adding an item on the first input, resulting in 1 letter items
-  -- instead, we need to be editing an item on input
-  -- then when you go to the blank box, you add a new blank item, and typing edits that item
+    { list | listItems = list.listItems ++ [ { id = (incrementItemId list), text = newItem, completed = False } ] }
+
+
+editItem : String -> Int -> ShoppingList -> ShoppingList
+editItem newItemText newItemId list =
+    let
+        applyIfEdited item =
+            if item.id == newItemId then
+                { item | text = newItemText }
+            else
+                item
+    in
+        { list | listItems = List.map applyIfEdited list.listItems }
 
 
 incrementItemId : ShoppingList -> Int
 incrementItemId list =
-  Maybe.withDefault 0 (List.maximum (List.map .id list.listItems)) + 1
-
+    Maybe.withDefault 0 (List.maximum (List.map .id list.listItems)) + 1
 
 
 updateShoppingList : Model -> ShoppingListId -> (ShoppingList -> ShoppingList) -> List ShoppingList
@@ -127,25 +136,28 @@ viewShoppingList list =
             ]
             []
         , div []
-            [ ul [ class "list" ] (List.concat [List.map viewListItem list.listItems, [viewAddListItem list]])
+            [ ul [ class "list" ] (List.concat [ List.map (viewListItem list.id) list.listItems, [ viewAddListItem list ] ])
             ]
         ]
 
 
-viewListItem : ListItem -> Html Msg
-viewListItem item =
+viewListItem : ShoppingListId -> ListItem -> Html Msg
+viewListItem listId item =
     li []
-        [ input [ placeholder "Item name", value item.text ] []
+        [ input [ placeholder "Item name", value item.text, onInput (ItemEdited listId item.id) ] []
         , input [ type_ "checkbox", name "list-item" ] []
         ]
 
+
 viewAddListItem : ShoppingList -> Html Msg
 viewAddListItem list =
-  li []
-      [ input
-          [ placeholder "Add a new list item"
-          , onInput (ItemAdded list.id)
-          ] [] ]
+    li []
+        [ input
+            [ placeholder "Add a new list item"
+            , onInput (ItemAdded list.id)
+            ]
+            []
+        ]
 
 
 subscriptions : Model -> Sub Msg
