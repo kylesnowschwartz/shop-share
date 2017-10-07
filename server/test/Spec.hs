181286a9573b6f95469a8aa1ced543fb08a205d5
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-10-07 19:29:36 +1300

    Add example specs & update test config

--- server/test/Spec.hs
+++ server/test/Spec.hs
@@ -1,31 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import           Control.Exception      (evaluate)
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Set               as Set
+import           Control.Exception (evaluate)
+import           Data.UUID.V4      (nextRandom)
+import           DB
 import           Test.Hspec
 import           Test.QuickCheck
 import           Types
 import           WsServer
 
 
+-- performAction :: Action -> IO LBS.ByteString
+
 main :: IO ()
-main = hspec $
-  describe "WsServer.updateState" $ do
+main = hspec $ before_ (runDB flushDb) $
+  describe "WsServer.performAction" $ do
     context "when action is GetLists" $ do
-      it "returns some lists" $ do
-        let expectedState = State Set.empty
-        updateState _ _ `shouldBe` expectedState
+      context "when there are no lists" $
+        it "returns no lists" $
+          performAction GetLists `shouldReturn` "{\"confirmAction\":{\"data\":{\"lists\":[]},\"type\":\"GetLists\"}}"
 
-      it "returns the first element of an *arbitrary* list" $
-        property $ \x xs -> head (x:xs) == (x :: Int)
-
-      it "throws an exception if used with an empty list" $
-        evaluate (head []) `shouldThrow` anyException
-
-    context "when action is CreateList" $ do
-      it "returns the first element of a list" $
-        head [23 ..] `shouldBe` (23 :: Int)
-
-      it "returns the first element of an *arbitrary* list" $
-        property $ \x xs -> head (x:xs) == (x :: Int)
+      context "when there is a list" $ do
+        it "returns the list" $ do
+          _ <- nextRandom >>= (runDB . insertList)
+          performAction GetLists `shouldReturn` "{\"confirmAction\":{\"data\":{\"lists\":[{\"createdAt\":\"2017-09-27T11:17:46.53297Z\",\"items\":[],\"listId\":\"77d640a1-6b9e-484d-ab36-4a8eaa7d8879\",\"updatedAt\":\"2017-09-27T11:17:46.53297Z\",\"title\":\"\"}]},\"type\":\"GetLists\"}}"
